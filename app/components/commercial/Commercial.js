277d51a0eacdf8a926c91888502371bf6681c536
Ryan King <ryan.mitchell.king@gmail.com>
2015-10-11 17:39:05 -0400

    Update commercial page text

--- app/components/commercial/Commercial.js
+++ app/components/commercial/Commercial.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
-import styles from './Commercial.scss';
+import './Commercial.scss';
 
 export default class Commercial extends React.Component {
   render() {
     return (
-      <div className={styles.about}>
-        <h1 className={styles.about__header}>About Ninja Ocean</h1>
-        <p className={styles.about__description}>Ninja Ocean is comprised of a team of passionate developers, hackers & scientists, aimed to do good.</p>
-      </div>
+        <div className="commercial-container">
+            <h1>Commercial options</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </div>
     );
   }
 }
--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,9 @@ import Route from 'react-router';
 
 import App from 'components/App';
 import Home from 'components/home/Home';
-import Residential from 'components/residential/Residential';
-import Commercial from 'components/commercial/Commercial';
-import Contact from 'components/contact/Contact';
+import Residential from 'components/residential/Residential.js';
+import Commercial from 'components/commercial/Commercial.js';
+import Contact from 'components/contact/Contact.js';
 
 export default (
   <Route component={App}>
